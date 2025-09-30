1662a167eb79691a03332ba5e390e89c090269a6
Sandip <rathodsandip0101@gmail.com>
2025-09-30 10:54:00 +0530

    "Adding log file for all pages"

--- JaggeryFactory.Infrastructure/obj/Release/net8.0/JaggeryAgro.Infrastructure.AssemblyInfo.cs
+++ JaggeryFactory.Infrastructure/obj/Release/net8.0/JaggeryAgro.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338c73b4f21cc40b943656a6f8e4ed9cba199a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe36978652038fe9958e2b68bb2217b40eeb3453")]
 [assembly: System.Reflection.AssemblyProductAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
