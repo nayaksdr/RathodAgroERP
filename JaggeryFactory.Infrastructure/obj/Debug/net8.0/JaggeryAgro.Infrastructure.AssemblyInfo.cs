5cc15be398ceacb35f35dd946feb98005c474fe9
Sandip <rathodsandip0101@gmail.com>
2025-10-01 22:16:30 +0530

    Updated payment summary UI with Bootstrap card and improved table design

--- JaggeryFactory.Infrastructure/obj/Debug/net8.0/JaggeryAgro.Infrastructure.AssemblyInfo.cs
+++ JaggeryFactory.Infrastructure/obj/Debug/net8.0/JaggeryAgro.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe36978652038fe9958e2b68bb2217b40eeb3453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1662a167eb79691a03332ba5e390e89c090269a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JaggeryAgro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
