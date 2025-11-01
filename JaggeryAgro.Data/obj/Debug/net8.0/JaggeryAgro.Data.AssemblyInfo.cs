55867a26b8a7fcc995b8278873240913f88dd177
Sandip <rathodsandip0101@gmail.com>
2025-11-01 23:03:01 +0530

    💰 Fixed payment summary totals and added Carry Forward total in CanePayment Index view some cane payment view

--- JaggeryAgro.Data/obj/Debug/net8.0/JaggeryAgro.Data.AssemblyInfo.cs
+++ JaggeryAgro.Data/obj/Debug/net8.0/JaggeryAgro.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1431db105cbe29b5b07ca2ae2123077020fbfc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f351540f0ee575be168b39f116aa9109f33ddb03")]
 [assembly: System.Reflection.AssemblyProductAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
