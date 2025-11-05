39f81e071bd6ebfe0c4a1cccbc1a46b64df181cb
Sandip <rathodsandip0101@gmail.com>
2025-11-05 17:49:30 +0530

    💰 Fixed Labor Type Rate Is not Getting Validate while Create it

--- JaggeryAgro.Data/obj/Debug/net8.0/JaggeryAgro.Data.AssemblyInfo.cs
+++ JaggeryAgro.Data/obj/Debug/net8.0/JaggeryAgro.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55867a26b8a7fcc995b8278873240913f88dd177")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c40711ed1d0bed42260d230fccd123d1e0c7628")]
 [assembly: System.Reflection.AssemblyProductAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JaggeryAgro.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
